ae2d3df569a0db3bd348293ae972164a96d5979d
kyungjae-Y <yoonkj2008@gmail.com>
2024-01-23 18:50:19 +0900

    .

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let button1 = document.querySelector('#to50')
 let button2 = document.querySelector('#snake')
 let button3 = document.querySelector('#tetris')
 let button4 = document.querySelector('#two')
+let button5 = document.querySelector('#wm')
 
 button1.addEventListener('click', () => {
   window.location.href = './game_1to50/1to50.html';
@@ -17,4 +18,8 @@ button3.addEventListener('click', () => {
 
 button4.addEventListener('click', () => {
   window.location.href = './game_2048/2048.html';
+})
+
+button5.addEventListener('click', () => {
+  window.location.href = './game_watermelon/watermelon.html';
 })
