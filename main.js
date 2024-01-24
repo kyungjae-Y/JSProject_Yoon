4914cb00ece695d54e00ce7618a2dfecd469cd4b
kyungjae-Y <yoonkj2008@gmail.com>
2024-01-24 17:57:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,27 +4,43 @@ let button3 = document.querySelector('#tetris')
 let button4 = document.querySelector('#two')
 let button5 = document.querySelector('#wm')
 let button6 = document.querySelector('#pp')
+let button7 = document.querySelector('.end')
+let opendWindow;
 
 button1.addEventListener('click', () => {
-  window.location.href = './game_1to50/1to50.html';
+  // window.location.href = './game_1to50/1to50.html';
+  opendWindow = window.open('./game_1to50/1to50.html')
 })
 
 button2.addEventListener('click', () => {
-  window.location.href = './game_snake/snake.html';
+  // window.location.href = './game_snake/snake.html';
+  opendWindow = window.open('./game_snake/snake.html')
 })
 
 button3.addEventListener('click', () => {
-  window.location.href = './game_tetris/tetris.html';
+  // window.location.href = './game_tetris/tetris.html';
+  opendWindow = window.open('./game_tetris/tetris.html')
 })
 
 button4.addEventListener('click', () => {
-  window.location.href = './game_2048/2048.html';
+  // window.location.href = './game_2048/2048.html';
+  opendWindow = window.open('./game_2048/2048.html')
 })
 
 button5.addEventListener('click', () => {
-  window.location.href = './game_watermelon/watermelon.html';
+  // window.location.href = './game_watermelon/watermelon.html';
+  opendWindow = window.open('./game_watermelon/watermelon.html')
 })
 
 button6.addEventListener('click', () => {
-  window.location.href = './game_pingpong/pingpong.html';
+  // window.location.href = './game_pingpong/pingpong.html';
+  opendWindow = window.open('./game_pingpong/pingpong.html')
+})
+
+button7.addEventListener('click', () => {
+  if (confirm('정말 그만하실건가요 ?') == true) {
+    window.close();
+  } else {
+    return;
+  }
 })
