445261b64c1256a114de63270a0b0ffaeeb20918
kyungjae-Y <yoonkj2008@gmail.com>
2024-01-24 16:10:02 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let button2 = document.querySelector('#snake')
 let button3 = document.querySelector('#tetris')
 let button4 = document.querySelector('#two')
 let button5 = document.querySelector('#wm')
+let button6 = document.querySelector('#pp')
 
 button1.addEventListener('click', () => {
   window.location.href = './game_1to50/1to50.html';
@@ -22,4 +23,8 @@ button4.addEventListener('click', () => {
 
 button5.addEventListener('click', () => {
   window.location.href = './game_watermelon/watermelon.html';
+})
+
+button6.addEventListener('click', () => {
+  window.location.href = './game_pingpong/pingpong.html';
 })
