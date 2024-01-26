867683712016e68303c514f67d05583637f2ccf7
kyungjae-Y <yoonkj2008@gmail.com>
2024-01-26 14:11:59 +0900

    Update home.js

--- home.js
+++ home.js
@@ -1,7 +1,6 @@
 let button1 = document.querySelector('#to50')
-let button2 = document.querySelector('#snake')
 let button3 = document.querySelector('#tetris')
-let button4 = document.querySelector('#two')
+let button4 = document.querySelector('#fm')
 let button6 = document.querySelector('#pp')
 let button7 = document.querySelector('.end')
 let opendWindow;
@@ -11,19 +10,14 @@ button1.addEventListener('click', () => {
   opendWindow = window.open('./game_1to50/1to50.html')
 })
 
-button2.addEventListener('click', () => {
-  // window.location.href = './game_snake/snake.html';
-  opendWindow = window.open('./game_snake/snake.html')
-})
-
 button3.addEventListener('click', () => {
   // window.location.href = './game_tetris/tetris.html';
   opendWindow = window.open('./game_tetris/tetris.html')
 })
 
 button4.addEventListener('click', () => {
-  // window.location.href = './game_2048/2048.html';
-  opendWindow = window.open('./game_2048/2048.html')
+  // window.location.href = './game_findmine/findmine.html';
+  opendWindow = window.open('./game_findmine/findmine.html')
 })
 
 button6.addEventListener('click', () => {
