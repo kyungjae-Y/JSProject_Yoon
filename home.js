b4fa5a16044b73cbb3d73116c7427cb7c1bc5a74
kyungjae-Y <yoonkj2008@gmail.com>
2024-01-26 18:25:13 +0900

    Update home.js

--- home.js
+++ home.js
@@ -1,9 +1,23 @@
+const myAudio = new Audio();
+myAudio.src = './메인테마.mp3';
+
+const btnPlay = document.getElementById('btnPlay');
+const btnPause = document.getElementById('btnPause');
+
 let button1 = document.querySelector('#to50')
 let button3 = document.querySelector('#tetris')
 let button4 = document.querySelector('#fm')
 let button6 = document.querySelector('#pp')
 let button7 = document.querySelector('.end')
 let opendWindow;
+
+btnPlay.onclick = function () {
+  myAudio.play();
+}
+
+btnPause.onclick = function () {
+  myAudio.pause();
+}
 
 button1.addEventListener('click', () => {
   // window.location.href = './game_1to50/1to50.html';
